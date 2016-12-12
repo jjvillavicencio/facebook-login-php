f069229182430f101e47d3c0f9588094959c86ee
John Villavicencio <jjvillavicencio1@gmail.com>
2016-12-12 17:17:44 -0500

    update index

--- index.php
+++ index.php
@@ -7,23 +7,23 @@
   //INICIAMOS UNA SESION PARA ADMINISTRAR EL USUARIO
   session_start();
   //CARGAMOS EL SDK DE FACEBOOK
-  require_once "php-graph-sdk-5.0.0/src/Facebook/autoload.php";
+  //require_once "php-graph-sdk-5.0.0/src/Facebook/autoload.php";
   //CARGAMOS LAS CREDENCIALES DE APLICACION DE FACEBOOK
   require_once "credentials.php";
 
  //CARGAR LA CONFIGURACIÓN DE LA APLICACIÓN EN EL SERVICIO FACEBOOK\FACEBOOK
- $fb = new Facebook\Facebook([
-   'app_id' => $app_id,
-   'app_secret' => $app_secret,
-   'default_graph_version' => 'v2.5'
-   ]);
+ //$fb = new Facebook\Facebook([
+ //  'app_id' => $app_id,
+ //  'app_secret' => $app_secret,
+ //  'default_graph_version' => 'v2.5'
+ //  ]);
 
  //INICIAR ASISTENTE PARA MANEHAR TOKEN DE ACCESO
- $helper = $fb->getRedirectLoginHelper();
+ //$helper = $fb->getRedirectLoginHelper();
  //DESCRIBIR LOS PERMISOS A LOS QUE QUIERES ACCEDER DEL USUARIO
- $permissions = ['email']; // permisos
+ //$permissions = ['email']; // permisos
  //GENERAR LA URL PARA SOLICITAR EL LOGIN
- $loginUrl = $helper->getLoginUrl($login_url, $permissions);
+ //$loginUrl = $helper->getLoginUrl($login_url, $permissions);
 
  echo '<a href="' . htmlspecialchars($loginUrl) . '">Entrar con Facebook!</a>';
 
